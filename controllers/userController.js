09af1694057b2d1ecd60a3e6abfa3d037af36633
ellysaschottler <ellysastork@gmail.com>
2024-01-24 14:25:22 -0600

    added delete user route and updated the singleUser route to populate the subdocs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const {User, Thought} = require('../models');
 
 module.exports = {
   async getUsers(req, res) {
@@ -11,7 +11,10 @@ module.exports = {
   },
   async getSingleUser(req, res) {
     try {
-      const user = await User.findOne({ _id: req.params.userId });
+      const user = await User.findOne({ _id: req.params.userId })
+        .select('-__v')
+        .populate('thoughts')
+        .populate('friends');
 
       if (!user) {
         return res.status(404).json({ message: 'No user with that ID' });
@@ -31,10 +34,40 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-};
+
 
 // to do: put - to update a user by its _id
-// to do : delete - to remove a user by its _id
-// to do bonus - remove a user's thoughts when deleted
+// to do : delete - to remove a user by its _id, bonus - remove a user's thoughts when deleted
+async deleteUser(req, res) {
+  try {
+    const user = await User.findOneAndRemove({ _id: req.params.userId });
+
+    if (!user) {
+      return res.status(404).json({ message: 'No user with this id!' });
+    }
+
+    const thought = await Thought.findOneAndUpdate(
+      { users: req.params.thoughtId },
+      { $pull: { thoughts: req.params.thoughtId } },
+      { new: true }
+    );
+
+    if (!thought) {
+      return res
+        .status(404)
+        .json({ message: 'No thoughts to delete' });
+    }
+
+    res.json({ message: 'Thought successfully deleted!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+},
+
+
 // to do post - add a new friend to a user's friend list
 // to do delete - remove a friend from a user's friend list
+
+
+//end bracket for exports
+};
