62ec894231bf56615c291bf15fc9560b9651b5e9
ellysaschottler <ellysastork@gmail.com>
2024-01-31 10:44:54 -0600

    changed user seed

--- utils/seed.js
+++ utils/seed.js
@@ -1,37 +1,21 @@
-const connection = require('../config/connection');
-const { Thought, User } = require('../models');
-const getRandomName = require('./data');
 
-console.log(getRandomName());
+const connection = require('../config/connection');
+const User = require('../models/User');
+const testUserData = require('./userseed.json');
+
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
-  // Delete the collections if they exist
-  let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
-  if (thoughtCheck.length) {
-    await connection.dropCollection('thoughts');
-  }
+  console.log('connected');
 
-  let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
-  if (userCheck.length) {
-    await connection.dropCollection('users');
-  }
+    const users = [{
+      "username": "tester",
+      "email": "tester@test.com"
+    }];
 
-  const users = [];
-
-  for (let i = 0; i < 20; i++) {
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-
-    users.push({
-      first,
-      last,
-      age: Math.floor(Math.random() * (99 - 18 + 1) + 18),
-    });
-  }
-
-  await User.collection.insertMany(users);
-  console.log(users);
-  process.exit(0);
+  await User.collection.insertMany(users)
+  console.table(users);
+  console.info('Seeding complete! 🌱');
+   process.exit(0);
 });
+
